5b690824d30a15c2dc87e895b0a96d847e60c787
Kareem James <kareemdjames@gmail.com>
2018-01-02 17:31:24 -0500

    created login screen, initially works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoginContainer from './components/LoginContainer'
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>Hello Kareem!</h1>
+      <div id="container" className="App inner-container">
+        <LoginContainer />
       </div>
     );
   }
