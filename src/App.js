67afad912c805c01a0dbe39e5c464bea6f0b9dda
Kareem James <kareemdjames@gmail.com>
2018-01-02 23:35:56 -0500

    messages saved in firebase and showing up in message feed with email address of user

--- src/App.js
+++ src/App.js
@@ -8,9 +8,11 @@ import UserContainer from './components/UserContainer'
 class App extends Component {
   constructor() {
     super()
-    this.state = { user: null }
+    this.state = {
+      user: null,
+      messages: []
+    }
   }
-
 
   componentDidMount() {
     window.firebase.auth().onAuthStateChanged((user) => {
@@ -22,13 +24,43 @@ class App extends Component {
           this.props.history.push('/login')
       }
     })
+    window.firebase
+      .database()
+      .ref('/messages')
+      .on('value', snapshot => {
+        this.onMessage(snapshot)
+      })
   }
+
+  onMessage = snapshot => {
+    const messages = Object.keys(snapshot.val()).map(key => {
+      const msg = snapshot.val()[key]
+      msg.id = key
+      return msg
+    })
+    this.setState({ messages })
+  }
+
+  handleSubmitMessage = msg => {
+    const data = {
+      msg,
+      author: this.state.user.email,
+      user_id: this.state.user.uid,
+      timestamp: Date.now()
+    }
+    window.firebase
+      .database()
+      .ref('messages/')
+      .push(data)
+  }
+
+
 
   render() {
     return (
-        <div id="container" className="App inner-container">
+        <div id="container" className="App">
           <Route path="/login" component={LoginContainer} />
-          <Route exact path="/" component={ChatContainer} />
+          <Route exact path="/" render={() => <ChatContainer onSubmit={this.handleSubmitMessage} messages={this.state.messages} user={this.state.user}/>} />
           <Route path="/users/:id" component={UserContainer} />
         </div>
     );
--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Header from './Header'
 
 class ChatContainer extends Component {
+
+  state = { newMessage: '' }
 
   handleLogout = () => {
     window.firebase.auth().signOut();
   }
 
+  handleInputChange = e => {
+    this.setState({ newMessage: e.target.value })
+  }
+
+  handleSubmit = () => {
+    this.props.onSubmit(this.state.newMessage)
+    this.setState({ newMessage: '' })
+  }
+
+  handleKeyDown = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      this.handleSubmit();
+    }
+  }
+
   render() {
     return (
-        <div id="ChatContainer">
+        <div id="ChatContainer" className="inner-container">
             <Header>
               <button className="red" onClick={this.handleLogout}>Logout</button>
             </Header>
-            <h1>Hello From ChatContainer</h1>
+          <div id="message-container">
+            {
+              this.props.messages.map(msg => (
+                <div key={msg.id} className={`message ${this.props.user.email === msg.author && 'mine'}`}>
+                  <p>{msg.msg}</p>
+                  <p className="author">
+                    <Link to={`/users/${msg.user_id}`}>{msg.author}</Link>
+                  </p>
+                </div>
+              ))
+            }
+          </div>
+          <div id="chat-input">
+            <textarea
+              onChange={this.handleInputChange}
+              onKeyDown={this.handleKeyDown}
+              value={this.state.newMessage}
+              placeholder="Add your message..." />
+            <button onClick={this.handleSubmit}>
+              <svg viewBox="0 0 24 24">
+                <path fill="#424242" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+              </svg>
+            </button>
+          </div>
         </div>
     )
 
